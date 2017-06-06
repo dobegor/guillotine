69ee8aef71e4e9d7627e377d17a24cac23f00c50
Egor Dobrovolsky <georgy.dobrovolsky@gmail.com>
2017-06-06 23:56:08 +0300

    added rasterization

--- solver/solver.go
+++ solver/solver.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/llgcode/draw2d/draw2dimg"
 	"github.com/rdarder/guillotine"
+	"image"
+	"image/color"
 	"log"
 	"math/rand"
 	"os"
@@ -18,12 +21,10 @@ type Solution struct {
 }
 
 func main() {
-	var nboards = flag.Int("nboards", 10, "Number of boards")
+	var nboards = flag.Int("nboards", 8, "Number of boards")
 	var population = flag.Int("population", 300, "Population size")
 	var tsize = flag.Int("tsize", 5, "Tournament size")
 	var eliteSize = flag.Int("eliteSize", 10, "Elite size")
-	var area = flag.Int("area", 2000, "Target total area")
-	var maxWidth = flag.Int("maxWidth", 0, "sheet max width")
 	var psel = flag.Float64("psel", 0.8, "Tournament selection probability")
 	var cx = flag.String("crossover", "uniform", "Crossover strategy")
 	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
@@ -31,7 +32,7 @@ func main() {
 		"Mean number of gene weights to be mutated on each individual")
 	var configMutateMean = flag.Float64("configMutateMean", 10,
 		"Mean number of pick configs to be mutated on each individual")
-	var generations = flag.Int("generations", 10, "Number of generations")
+	var generations = flag.Int("generations", 1000, "Number of generations")
 	var seed = flag.Int64("seed", time.Now().Unix(), "Random seed for repeatable runs")
 
 	flag.Parse()
@@ -58,22 +59,20 @@ func main() {
 	}
 
 	r := rand.New(rand.NewSource(*seed))
-	var width, height uint
-	var limitWidth bool
 
-	if *maxWidth == 0 {
-		width, height = guillotine.AreaDimensions(float64(*area), r)
-		limitWidth = false
-	} else {
-		width, height = guillotine.MaxWidthDimensions(*maxWidth, r)
-		limitWidth = true
-	}
-	spec := guillotine.NewRandomSpec(*nboards, width, height, r, limitWidth)
-	target := width * height
+	spec := &guillotine.CutSpec{}
+	spec.MaxWidth = 500
+	spec.Add(200, 100).
+		Add(150, 100).
+		Add(50, 50).
+		Add(50, 70).
+		Add(150, 100).
+		Add(50, 50).
+		Add(50, 70).Add(123, 231)
 
 	ga := &guillotine.GeneticAlgorithm{
-		Spec:        spec,
-		Evaluator:   (*guillotine.LayoutTree).Area,
+		Spec:      spec,
+		Evaluator: (*guillotine.LayoutTree).Area,
 		Mutator: guillotine.CompoundWeightConfigMutator{
 			Weight: guillotine.NormalWeightMutator{
 				Mean:   *weightMutateMean,
@@ -98,11 +97,39 @@ func main() {
 
 	bestLayout := guillotine.GetPhenotype(spec, rankedPop.Pop[0])
 	drawer := guillotine.NewDrawer(bestLayout)
-	b, err := json.Marshal(drawer.Draw())
+	drawing := drawer.Draw()
+	b, err := json.Marshal(drawing)
 	if err != nil {
 		log.Fatal("error:", err)
 	}
 	os.Stdout.Write(b)
-	best := rankedPop.Fitnesses[0]
-	fmt.Printf("\nWaste: %.2f%%\n", 100*(float32(best)/float32(target)-1))
+
+	fmt.Printf("\nWaste: %v\n", 100*float64(drawing.Sheet.Width*drawing.Sheet.Height-spec.TotalArea)/float64(spec.TotalArea))
+
+	dest := image.NewRGBA(image.Rect(0, 0, int(drawing.Sheet.Width), int(drawing.Sheet.Height)))
+	gc := draw2dimg.NewGraphicContext(dest)
+
+	// Set some properties
+	gc.SetFillColor(color.RGBA{0xff, 0xff, 0xff, 0xff})
+	//gc.SetStrokeColor(color.RGBA{0x44, 0x44, 0x44, 0xff})
+	gc.SetStrokeColor(color.RGBA{0xff, 0x00, 0x00, 0xff})
+
+	gc.SetLineWidth(5)
+	gc.DPI *= 10
+
+	for _, rect := range drawing.Boxes {
+		gc.MoveTo(float64(rect.X), float64(rect.Y))
+		gc.LineTo(float64(rect.X+rect.Width), float64(rect.Y))
+		gc.MoveTo(float64(rect.X+rect.Width), float64(rect.Y))
+		gc.LineTo(float64(rect.X+rect.Width), float64(rect.Y+rect.Height))
+		gc.MoveTo(float64(rect.X+rect.Width), float64(rect.Y+rect.Height))
+		gc.LineTo(float64(rect.X), float64(rect.Y+rect.Height))
+		gc.MoveTo(float64(rect.X), float64(rect.Y+rect.Height))
+		gc.LineTo(float64(rect.X), float64(rect.Y))
+		gc.Close()
+		gc.FillStroke()
+	}
+
+	// Save to file
+	draw2dimg.SaveToPngFile("output.png", dest)
 }
